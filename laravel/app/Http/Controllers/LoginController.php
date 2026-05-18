34288c36306501d1d1d3f8f195c58a26eb774606
SeanSSpu <temerlantastybaev12@gmail.com>
2026-05-18 08:47:02 +0000

    {312}

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+     public function show(){
+        return view('auth.login');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            "email"=>"required|email",
+            "password"=>"required|string"
+        ]);
+
+        if (!Auth::attempt($data,$request->boolean('remember'))){
+            return back()->withErrors([
+                'email'=> 'Неправильный email или пароль'
+            ]);
+        }
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('dashboard'));
+    }
+   
 }
